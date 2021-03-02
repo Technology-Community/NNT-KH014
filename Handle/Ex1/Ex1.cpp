a79fd94af83e678e2a1c9e0e49ef5f9ad9643b78
KingNNT <Dev.KingNNT@gmail.com>
2021-03-02 23:00:59 +0700

    Done Ex1

--- Handle/Ex1/Ex1.cpp
+++ Handle/Ex1/Ex1.cpp
@@ -1,16 +1,4 @@
-/**/
-/* Code by KingNNT */
-
-/* Include File */
 #include <bits/stdc++.h>
-// #include ""
-
-/* Define */
-
-#define pathio "/mnt/LearningAndWorking/Develop/C-CPP/GNU-Compiler/In-Out/"
-// #define pathio "E:/Develop/C-CPP/GNU-Compiler/In-Out/"
-#define lfi pathio "input.txt", "r", stdin
-#define lfo pathio "output.txt", "w", stdout
 
 using namespace std;
 
@@ -191,14 +179,6 @@ void solve()
 
 int main()
 {
-    freopen(lfi);
-    freopen(lfo);
-    // ios_base :: sync_with_stdio (0);
-    cin.tie(0);
-
     solve();
-
-    fclose(stdin);
-    fclose(stdout);
     return 0;
 }
