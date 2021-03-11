7f14ed5078f93eee7a7ea1d8b0aef43838cc4bb4
KingNNT <Dev.KingNNT@gmail.com>
2021-03-11 15:04:40 +0700

    Init Ex9

---
+++ Solve/Ex9/main.cpp
@@ -0,0 +1,119 @@
+/* Ex1 - Page 16 */
+/*
+    Descriptions
+*/
+
+/* Code by KingNNT */
+/*
+Author's infomations:
+    - Email: Dev.KingNNT@gmail.com
+    - Skype: live:king.nnt
+    - Facebook Profile: https://www.facebook.com/Kinggg.NNT
+    - Facebook Page: https://www.facebook.com/Dev.KingNNT
+    - Github Profile: https://github.com/KingNNT
+    - Gitlap Profile: https://gitlab.com/Dev.KingNNT
+    - Bitbucket Profile:
+*/
+
+/* ---------- | ---------- | ---------- | ---------- | ---------- */
+
+/* INCLUDE FILE */
+/** Include libraries of CPP */
+#include <bits/stdc++.h>
+/** Include my libraries */
+// #include ""
+/* END INCLUDE FILE */
+
+/* ---------- | ---------- | ---------- */
+/* DEFINITIONS */
+/** Define path file for input and output */
+#define pathio "/mnt/LearningAndWorking/Develop/C-CPP/GNU-Compiler/In-Out/"
+// #define pathio "E:/Develop/C-CPP/GNU-Compiler/In-Out/"
+#define lfi pathio "input.txt", "r", stdin
+#define lfo pathio "output.txt", "w", stdout
+
+/** Macro definitions for maximum length of variables */
+
+/* END DEFINITIONS */
+
+/* ---------- | ---------- | ---------- */
+/* NAMESPACE */
+using namespace std;
+/* END NAMESPACE */
+
+/* ---------- | ---------- | ---------- | ---------- | ---------- */
+
+/* DECLARE FUNCTIONS HELPER */
+/*** Code at "DECLARE FUNCTION HELPER BLOCK" ***/
+
+/* END DECLARE FUNCTIONS HELPER */
+
+/* ---------- | ---------- | ---------- */
+/* DECLARE CLASS */
+/*** Code at "DECLARE CLASS BLOCK" ***/
+
+/* END DECLARE CLASS */
+
+/* ---------- | ---------- | ---------- */
+/* DECLARE STRUCT */
+/*** Code at "DECLARE STRUCT BLOCK" ***/
+
+/* END DECLARE STRUCT */
+
+/* ---------- | ---------- | ---------- */
+/* DECLARE FUNCTIONS HANDLE*/
+/*** Code at "DECLARE FUNCTION HANDLE BLOCK" ***/
+
+/* END DECLARE FUNCTIONS HANDLE */
+
+/* ---------- | ---------- | ---------- | ---------- | ---------- */
+
+/* SOLVE */
+void solve()
+{
+    // Code Here
+    return;
+}
+/* END SOLVE */
+
+/* ---------- | ---------- | ---------- */
+/* MAIN */
+int main()
+{
+    freopen(lfi);
+    freopen(lfo);
+    // ios_base :: sync_with_stdio (0);
+    cin.tie(0);
+
+    solve();
+
+    fclose(stdin);
+    fclose(stdout);
+    return 0;
+}
+/* END MAIN*/
+
+/* ---------- | ---------- | ---------- | ---------- | ---------- */
+
+/* FUNCTIONS HELPER */
+/*** Declare at "DECLARE FUNCTION HELPER BLOCK" ***/
+
+/* END FUNTIONS HELPER */
+
+/* ---------- | ---------- | ---------- */
+/* FUNCTIONS HANDLE*/
+/*** Declare at "DECLARE FUNCTION HANDLE BLOCK" ***/
+
+/* END FUNTIONS HANDLE */
+
+/* ---------- | ---------- | ---------- */
+/* STRUCT */
+/*** Declare at "DECLARE STRUCT BLOCK" ***/
+
+/* END STRUCT */
+
+/* ---------- | ---------- | ---------- */
+/* CLASS */
+/*** Declare at "DECLARE CLASS BLOCK" ***/
+
+/* END CLASS */
